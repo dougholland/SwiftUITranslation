62114d49fca6751296702225e3d36c67ff93217f
Doug Holland <doug.holland@outlook.com>
2024-10-03 11:21:49 -0700

    Completed the simple translation and tested on an iPhone 15 Pro Max running iOS 18.0

--- SwifUITranslation/SimpleTranslationView.swift
+++ SwifUITranslation/SimpleTranslationView.swift
@@ -6,10 +6,57 @@
 //
 
 import SwiftUI
+import Translation
 
 struct SimpleTranslationView: View {
+    @State private var text: String = ""
+    
+    @State private var translatedText: String = ""
+    
+    @State private var configuration: TranslationSession.Configuration?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("enter text to translate", text: $text, axis: .vertical)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .frame(minHeight: 200)
+            
+            Button(action: {
+                Task {
+                    try await performTranslation()
+                }
+            }) {
+                Text("Translate")
+                    .frame(minWidth: 80, maxWidth: .infinity)
+                    .padding()
+                    .cornerRadius(8)
+            }
+            .padding()
+            
+            TextField("translated text", text: $translatedText, axis: .vertical)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .frame(minHeight: 200)
+                
+        }
+        .translationTask(configuration) { session in
+            do {
+                let response = try await session.translate(text)
+                
+                translatedText = response.targetText
+            } catch {
+                // TODO: handle errors
+            }
+        }
+    }
+    
+    func performTranslation() async throws {
+        if configuration == nil {
+            configuration = TranslationSession.Configuration()
+        }
+        
+        configuration?.invalidate()
     }
 }
 
