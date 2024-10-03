72e7aca30e3fecbe8cbdb224ad4001e750092806
Doug Holland <doug.holland@outlook.com>
2024-10-03 12:14:21 -0700

    Completed the batch translation and tested on an iPhone 15 Pro Max running iOS 18.0

--- SwifUITranslation/BatchTranslationView.swift
+++ SwifUITranslation/BatchTranslationView.swift
@@ -6,11 +6,78 @@
 //
 
 import SwiftUI
+import Translation
 
 struct BatchTranslationView: View {
+    @State private var task: String = ""
+    
+    @State private var tasks: [String] = []
+    
+    @State private var configuration: TranslationSession.Configuration?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("enter a new task", text: $task, axis: .vertical)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .frame(minHeight: 200)
+            
+            Button(action: {
+                if !task.isEmpty {
+                    tasks.append(task)
+                    task = ""
+                }
+            }) {
+                Text("Add Task")
+                    .frame(minWidth: 80, maxWidth: .infinity)
+                    .padding()
+                    .cornerRadius(8)
+            }
+            
+            Button(action: {
+                Task {
+                    try await performTranslation()
+                }
+            }) {
+                Text("Translate")
+                    .frame(minWidth: 80, maxWidth: .infinity)
+                    .padding()
+                    .cornerRadius(8)
+            }
+            .padding()
+            
+            List(tasks, id: \.self) { task in
+                Text(task)
+            }
+        }
+        .translationTask(configuration) { session in
+            let requests: [TranslationSession.Request] = tasks.map {
+                TranslationSession.Request(sourceText: $0)
+            }
+            
+            do {
+                let responses = try await session.translations(from: requests)
+                
+                tasks = responses.map {
+                    $0.targetText
+                }
+            } catch {
+                // TODO: handle errors
+            }
+        }
     }
+    
+    func performTranslation() async throws {
+        if configuration == nil {
+            configuration = TranslationSession.Configuration()
+        }
+        
+        configuration?.invalidate()
+    }
+    
+    //func handle(_ response: TranslationSession.BatchResponse) {
+        
+    //}
 }
 
 #Preview {
