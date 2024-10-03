da815aa5c989ad4a0fd2d9ec7dbad44c5a196b00
Doug Holland <doug.holland@outlook.com>
2024-10-03 10:12:29 -0700

    Initial Commit

---
+++ SwifUITranslation/SwifUITranslationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUITranslationApp.swift
+//  SwifUITranslation
+//
+//  Created by Doug Holland on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwifUITranslationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
